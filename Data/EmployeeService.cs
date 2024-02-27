61ac15738f663a1433ab59cfd221d286a6406df1
DESKTOP-PC6NOLF\ahsan <ahsanshafqat@gmail.com>
2024-02-27 14:48:24 +0000

    added employee list

---
+++ Data/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeBlazorCRUD.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeBlazorCRUD.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public EmployeeService(ApplicationDbContext applicationDbContext) {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+
+        
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await applicationDbContext.Employees.ToListAsync();
+        }
+    }
+}
---
+++ DataContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeBlazorCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeBlazorCRUD.DataContext
+{
+    public class ApplicationDbContext  : DbContext
+    {
+        public  ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
